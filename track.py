8598c7ea02a3e54e79b82fedf6853f3f34c92bc9
celik67rafet <rafet.celikk.07@gmail.com>
2026-06-21 13:59:16 +0300

    feat: daha karmasik u ve l virajli yeni pist eklendi

--- track.py
+++ track.py
@@ -1,213 +1,131 @@
 # pygame kütüphanesini içe aktar:
 import pygame
-
-# math import
 import math
-
-# Rastgele seçim için:
 import random
 
 class Track:
     # Pist sınıfını başlat:
-    def __init__( self ):
-        # Pist dış sınıır:
-        self.outer_rect = pygame.Rect( 140,80,1000,560 )
-        
-        # Checkpoint çizgileri
+    def __init__(self):
+
+        # Yeni ve daha karmaşık pisti oluşturan yol parçaları ( dikdörtlenler ):
+        # Sol alt köşeden başlayıp dolambaçlı bir tur atıyoruz.
+        self.road_rects = [
+            pygame.Rect(100, 500, 900, 150), # 1. Alt düzlük
+            pygame.Rect(850, 100, 150, 550), # 2. Sağ düzlük ( Yukarı çıkış )
+            pygame.Rect(400, 100, 600, 150), # 3. Üst düzlük ( Sola gidiş )
+            pygame.Rect(400, 100, 150, 300), # 4. Orta dikey ( aşağı iniş )
+            pygame.Rect(100, 250, 450, 150), # 5. Orta Dikey ( Sola gidiş )
+            pygame.Rect(100, 250, 150, 400), # 6. Sol dikey ( Başlangıç dönüş )
+
+        ]
+
+        # Yeni piste uygun checkpoint'ler ( Sırasıyla geçilmesi gereken çizgiler )
         self.checkpoints = [
-            ((140, 300), (340, 300)),    # Sol orta / başlangıç sonrası
-            ((450, 80), (450, 220)), 
-            ((800, 80), (800, 220)),
-            ((940, 300), (1135, 300)),
-            ((800, 420), (800, 635)),
-            ((450, 420), (450, 635)), 
+            (( 300, 500 ), ( 300, 650 )), # 1. Alt düzlük ortası
+            (( 650, 500 ), ( 650, 650 )), # 2. Alt düzlük sonu 
+            (( 850, 400 ), ( 1000, 400 )), # 3. Sağ düzlük tırmanış
+            (( 850, 250 ), ( 1000, 250 )), # 4. Sağ düzlük üst
+            (( 650, 100 ), ( 650, 250 )), # 5. Üst düzlük dönüşü
+            (( 400, 246 ), ( 550, 250 )), # 6. Orta dikey iniş
+            (( 300, 250 ), ( 300, 400 )), # 7. Orta yatay
+            (( 100, 450 ), ( 250, 450 )), # 8. Sol dikey iniş
         ]
 
-        # Alternatif başlangıç noktaları ( x, y ,açı ):
-        self.start_positions = [
-            ( 220, 360, 270 ), # Orjinal başlangıç noktamız
-            ( 260, 360, 270 ), # Biraz daha sağda
-            ( 180, 360, 270 ), # Biraz daha solda
-            ( 220, 380, 270 ), # Biraz daha geride
-        ]
-
-        # Checkpoint algılama yarıçağı
+        # Checkpoint algılama yarıçapı
         self.checkpoint_radius = 30
 
-        # Pist iç boşluğu
-        self.inner_rect = pygame.Rect( 340, 220, 600, 200 )
+        # Yeni başlangıç noktaları ( Alt düzlüğün başında, sağa ( açı: 0 ) bakacak şekilde )
+        self.start_positions = [
+            ( 180, 550, 0 ),
+            ( 180, 600, 0 ),
+            ( 150, 575, 0 ),
+            ( 200, 575, 0 )
+        ]
 
-    # Pisti ekrana çiz:
-    def draw( self, screen, target_ceheckpoint_index ):
-        # Tüm arka planı çim alan gibi yeşil çiz:
-        screen.fill(( 35,120,55 ))
-
-        # Pist dış alanını gri çiz:
-        pygame.draw.rect( screen, ( 90, 90, 90 ), self.outer_rect )
-
-        # Pist iç boşluğu tekrar yeşil çiz
-        pygame.draw.rect( screen, ( 35, 120, 55 ), self.inner_rect )
-
-        # Pist dış kenar çizgisi:
-        pygame.draw.rect( screen, ( 230, 230, 230 ), self.outer_rect, 4 )
-
-        # Pist iç kenar çizgisi:
-        pygame.draw.rect( screen, ( 230, 230, 230 ), self.inner_rect, 4 )
-
-        # Tüm checkpointleri çiz:
-        for index, checkpoint in enumerate( self.checkpoints ):
-
-            # Varsayılan checkpoint rengi:
-            color = ( 255, 255 , 0 )
-
-            # Hedef checkpoint'i yeşil göster:
-            if index == target_ceheckpoint_index:
-                color = ( 0, 255, 0 )
-
-            # Çizginin başlangıç noktası:
-            start_point = checkpoint[0]
-
-            # Çizginin bitiş noktası:
-            end_point = checkpoint[1]
-
-            #Checkpoint noktasını çiz:
-            pygame.draw.line(
-                screen,
-                color,
-                start_point,
-                end_point,
-                4
-            )
-
-    def is_on_track( self, x, y ):
-        # Nokta dış pist dikdörtgeninin içinde mi?
-        inside_outer = self.outer_rect.collidepoint( x, y )
-
-        # Nokta iç boşluk dikdörtgeninin içinde mi?
-        inside_inner = self.inner_rect.collidepoint( x, y )
-
-        # Pistte olmak için dış sınır içinde ve iç boşluk dışında olmalı:
-        return inside_outer and not inside_inner
+    # Listeden rastgele bir başlangıç pozisyonu seç:
+    def get_random_start_position( self ):
+        return random.choice( self.start_positions )
     
+    # Pisti ekrana çiz:
+    def draw( self, screen, target_checkpoint_index ):
 
+        # Arka plan çim ( Yeşil )
+        screen.fill(( 35, 120, 55 ))
+
+        # Tüm yol parçalarını gri çiz:
+        for rect in self.road_rects:
+            pygame.draw.rect( screen, ( 90, 90, 90 ), rect )
+
+        # Checkpointleri çiz:
+        for index, self.checkpoint in enumerate( self.checkpoints ):
+
+            color = ( 255, 255, 0 ) # Normal Checkpoint sarı
+            if index == target_checkpoint_index:
+                color = ( 0, 255 ,0 ) # Hedef checkpoint yeşil
+
+            pygame.draw.line( screen, color, self.checkpoint[0], self.checkpoint[1], 4 )
+
+    # Koordinat pist üzerinde mi kontrol et:
+    def is_on_track( self, x, y ):
+        # Nokta yol parçalarında ( dikdörtgenlerden ) HERHANGİ BİRİNİN içindeyse pisttedir:
+        for rect in self.road_rects:
+            if rect.collidepoint( x, y ):
+                return True
+        return False
+    
     # Aracın tamamı pist üzerinde mi kontrol et:
     def is_car_on_track( self, car ):
-         # Aracın dört köşesini al:
         corners = car.get_corners()
-
-        # Her köşeyi tek tek kontrol et:
-        for x,y in corners:
-
-            # Herhangi bir köşe pist dışında ise araç pist dışındadır.
-            if not self.is_on_track( x,y ):
+        for x, y in corners:
+            if not self.is_on_track( x, y ):
                 return False
-            
-        # Tüm köşeler pist üzerindeyse araç pisttedir:
         return True
     
-    # Aracın tüm sensörlerinin piste çıkmadan önceki mesafesini ölç:
+    # Aracın tüm sensörlerinin mesafesini ölç:
     def get_sensor_distances( self, car ):
-        # Ölçülen sensör mesafeleri:
         distances = []
-
-        #Her sensör açısını sırayla işle:
         for sensor_angle in car.sensor_angles:
-
-            # Sensörün dünya açısını hesapla:
             angle = car.angle + sensor_angle
-
-            # Sensörün bakış açısını radyana çevir
             radians = math.radians( angle )
-
-            # Varsayılan mesafe maksimum sensör uzunluğu:
             measured_distance = car.sensor_length
 
-            # Sensörü piksel piksel ilerlet:
             for distance in range( car.sensor_length ):
-
-                # Sensör üzerindeki test noktasının x konumu:
                 test_x = car.x + math.cos( radians ) * distance
-
-                # Sensör üzerindeki test noktasının y konumu:
                 test_y = car.y + math.sin( radians ) * distance
 
-                # Test notkası pist dışına çıktıysa mesafeyi kaydet:
                 if not self.is_on_track( test_x, test_y ):
                     measured_distance = distance
                     break
-
-            # Ölçülen mesafeyi listeye ekle:
             distances.append( measured_distance )
-
-        # Sol, ön, sağ sensör mesafelerini döndür:
         return distances
     
-    #Araç belirtilen checkpoint'e ulaştı mı?
+    # Araç checkpoint'e ulaştı mı:
     def reached_checkpoint( self, car, checkpoint ):
-        # Çizginin başlangıç noktası:
-        x1,y1 = checkpoint[0]
-
-        # Çizginin bitiş noktası:
-        x2,y2 = checkpoint[1]
-
-        # Araç merkezinin koordinatları:
-        px = car.x
-        py = car.y
-
-        # Çizginin vektörü:
-        dx = x2 - x1
-        dy = y2 - y1
-
-        # Çizgi uzunluğunun karesi:
+        x1, y1 = checkpoint[0]
+        x2, y2 = checkpoint[1]
+        px, py = car.x, car.y
+        dx, dy = x2 - x1, y2 - y1
         line_length_squared = dx * dx + dy * dy
 
-        # Güvenlik kontrolü:
         if line_length_squared == 0:
             return False
         
-        # Araç merkezinin çizgi üzerindeki en yakın noktasını bul:
-        t = (
-            (( px - x1 ) * dx + ( py - y1 ) * dy)
-            / line_length_squared
-        )
+        t = (((px - x1) * dx + (py - y1) * dy) / line_length_squared)
+        t = max(0, min(1, t))
 
-        # Çizgi segmenti içinde sınırla:
-        t = max( 0, min( 1, t ) )
-
-        # En yakın noktanın koordinatları:
         nearest_x = x1 + t * dx
         nearest_y = y1 + t * dy
 
-        # Araç ile çizgi arasındaki mesafe:
-        distance = math.sqrt(
-            ( px - nearest_x ) ** 2
-            + ( py - nearest_y ) ** 2
-        )
-
-        # Çizgiye yeterince yakınsa checkpoint geçilmiş say:
+        distance = math.sqrt((px - nearest_x) ** 2 + (py - nearest_y) ** 2)
         return distance <= 25
     
-    # Araç ile checkpoint çizgisinin merkezi arasındaki mesafeyi hesapla:
+    # Hedef checkpoint'e olan mesafeyi hesapla:
     def get_checkpoint_distance( self, car, checkpoint ):
-        # Çizginin başlangıç noktası:
         x1, y1 = checkpoint[0]
-
-        # Çizginin bitiş noktası:
         x2, y2 = checkpoint[1]
-
-        # Checkpoint çizgisinin merkez x konumu:
-        center_x = ( x1 + x2 ) / 2
-
-        #Checkpoint çizgisinin merkez y konumu:
-        center_y = ( y1 + y2 ) / 2
-
-        # Araç ile checkpoint merkezi arasındaki fark:
+        center_x = (x1 + x2) / 2
+        center_y = (y1 + y2) / 2
         dx = car.x - center_x
         dy = car.y - center_y
 
-        # Mesafeyi döndür:
         return ( dx * dx + dy * dy ) ** 0.5
-    
-    # Listeden rastgele bir başlangıç pozisyonu seçip döndür:
-    def get_random_start_position( self ):
-        return random.choice( self.start_positions )
